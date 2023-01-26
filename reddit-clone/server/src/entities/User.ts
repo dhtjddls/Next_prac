18bb9816fe4cf118a739761a645b9dd14e3a6ed5
오성인 <dhtjddls12@naver.com>
2023-01-26 21:24:28 +0900

    feat:add user entitiy

--- reddit-clone/server/src/entities/User.ts
+++ reddit-clone/server/src/entities/User.ts
@@ -1,18 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { IsEmail, Length } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column, Index } from "typeorm";
 
-@Entity()
+@Entity("users")
 export class User {
+  @Index()
+  @IsEmail(undefined, { message: "이메일 주소가 잘못되었습니다." })
+  @Length(1, 255, { message: "이메일 주소는 비워둘 수 없습니다." })
+  @Column({ unique: true })
+  email: string;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Index()
+  @Length(3, 32, { message: "사용자 이름은 3자 이상이어야 합니다." })
+  @Column()
+  firstName: string;
 
-    @Column()
-    firstName: string
+  @Column()
+  lastName: string;
 
-    @Column()
-    lastName: string
-
-    @Column()
-    age: number
-
+  @Column()
+  age: number;
 }
